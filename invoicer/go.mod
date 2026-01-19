555f1555aeda90a648098577116e589369c77fad
Aman Saxena <amansaxena2809@gmail.com>
2026-01-20 04:32:15 +1100

    Configure CORS AllowedOrigins from env

--- invoicer/go.mod
+++ invoicer/go.mod
@@ -3,7 +3,8 @@ module invoice-generator/invoicer
 go 1.24.5
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
-	github.com/rs/cors v1.11.1 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/rs/cors v1.11.1
 )
--- invoicer/main.go
+++ invoicer/main.go
@@ -6,11 +6,18 @@ import (
 	"log"
 	"net/http"
 
+	"os"
+	"strings"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func main() {
+	// Load .env file if it exists
+	_ = godotenv.Load()
+
 	// Create router
 	router := mux.NewRouter()
 
@@ -21,9 +28,18 @@ func main() {
 	router.HandleFunc("/api/generate-pdf", invoiceHandler.GeneratePDF).Methods("POST", "OPTIONS")
 	router.HandleFunc("/health", invoiceHandler.HealthCheck).Methods("GET")
 
+	// Get allowed origins from environment
+	allowedOriginsEnv := os.Getenv("ALLOWED_ORIGINS")
+	var allowedOrigins []string
+	if allowedOriginsEnv != "" {
+		allowedOrigins = strings.Split(allowedOriginsEnv, ",")
+	} else {
+		allowedOrigins = []string{"http://localhost:5173", "http://localhost:3000"}
+	}
+
 	// Setup CORS
 	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowedOrigins:   allowedOrigins,
 		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
